5dee78b2f3f0a313f927ae7a07f5df84314f5289
Leonardo Bugoni <leobugoni@gmail.com>
2020-09-03 09:26:35 -0300

    Initial Commit

---
+++ src/main/java/io/github/leobugoni/kafkadeadlettertopicretry/KafkaDeadLetterTopicRetryApplication.java
@@ -0,0 +1,13 @@
+package io.github.leobugoni.kafkadeadlettertopicretry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDeadLetterTopicRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDeadLetterTopicRetryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leobugoni/kafkadeadlettertopicretry/KafkaDeadLetterTopicRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leobugoni.kafkadeadlettertopicretry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDeadLetterTopicRetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
